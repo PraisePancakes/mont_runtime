0c7979e28dc7c9e057636a32544f7dfa262f38a5
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2024-07-25 19:13:03 -0400

    implemented more token types

--- src/frontend/tokenizer.hpp
+++ src/frontend/tokenizer.hpp
@@ -16,13 +16,37 @@ namespace MPROCESS
         TOK_LCURLY,
         TOK_RPAREN,
         TOK_LPAREN,
-        TOK_BANG_EQUALS,
         TOK_EQUALS,
         TOK_SEMI,
         TOK_COMMA,
         TOK_INT_LIT,
         TOK_STRING_LIT,
         TOK_IDENTIFIER,
+        TOK_BANG,
+        TOK_BANG_EQUALS,
+        TOK_EQUAL_EQUALS,
+        TOK_GREATER,
+        TOK_GREATER_EQUALS,
+        TOK_LESS,
+        TOK_LESS_EQUALS,
+        TOK_CLASS,
+        TOK_AND,
+        TOK_IF,
+        TOK_ELSE,
+        TOK_FN,
+        TOK_FOR,
+        TOK_NULL,
+        TOK_OR,
+        TOK_PRINT,
+        TOK_RETURN,
+        TOK_SUPER,
+        TOK_THIS,
+        TOK_TRUE,
+        TOK_FALSE,
+        TOK_VAR,
+        TOK_WHILE,
+        TOK_ENTRY,
+        TOK_EOF,
         TOK_ERROR,
 
     };
@@ -44,50 +68,129 @@ namespace MPROCESS
             for (size_t i = 0; i < lexemes.size(); i++)
             {
                 LexemeItem lexeme_item(lexemes[i]);
+                TOKEN_TYPE type = TOKEN_TYPE::TOK_ERROR;
 
                 if (lexemes[i].lexeme == "{")
                 {
-                    tokens.push_back({TOKEN_TYPE::TOK_LCURLY, lexeme_item});
+                    type = TOKEN_TYPE::TOK_LCURLY;
                 }
                 if (lexemes[i].lexeme == "}")
                 {
-                    tokens.push_back({TOKEN_TYPE::TOK_RCURLY, lexeme_item});
+                    type = TOKEN_TYPE::TOK_RCURLY;
                 }
                 if (lexemes[i].lexeme == "(")
                 {
-                    tokens.push_back({TOKEN_TYPE::TOK_LPAREN, lexeme_item});
+                    type = TOKEN_TYPE::TOK_LPAREN;
                 }
                 if (lexemes[i].lexeme == ")")
                 {
-                    tokens.push_back({TOKEN_TYPE::TOK_RPAREN, lexeme_item});
+                    type = TOKEN_TYPE::TOK_RPAREN;
                 }
                 if (lexemes[i].lexeme == ",")
                 {
-                    tokens.push_back({TOKEN_TYPE::TOK_COMMA, lexeme_item});
+                    type = TOKEN_TYPE::TOK_COMMA;
                 }
 
                 if (lexemes[i].lexeme == "+")
                 {
-                    tokens.push_back({TOKEN_TYPE::TOK_ADD, lexeme_item});
+                    type = TOKEN_TYPE::TOK_ADD;
                 }
 
                 if (lexemes[i].lexeme == ";")
                 {
-                    tokens.push_back({TOKEN_TYPE::TOK_SEMI, lexeme_item});
+                    type = TOKEN_TYPE::TOK_SEMI;
                 };
 
                 if (lexemes[i].lexeme == "\"")
                 {
-                    tokens.push_back({TOKEN_TYPE::TOK_STRING_LIT, lexeme_item});
+                    type = TOKEN_TYPE::TOK_STRING_LIT;
+                    tokens.push_back({type, lexeme_item});
                     i++;
                     while (lexemes[i].lexeme != "\"")
                     {
                         LexemeItem string_lit(lexemes[i]);
-                        tokens.push_back({TOKEN_TYPE::TOK_STRING_LIT, string_lit});
+                        tokens.push_back({type, string_lit});
                         i++;
                     }
-                    tokens.push_back({TOKEN_TYPE::TOK_STRING_LIT, lexeme_item});
                 }
+
+                if (lexemes[i].lexeme == "=")
+                {
+
+                    type = TOKEN_TYPE::TOK_EQUALS;
+                }
+
+                if (lexemes[i].lexeme == "==")
+                {
+
+                    type = TOKEN_TYPE::TOK_EQUAL_EQUALS;
+                }
+
+                if (lexemes[i].lexeme == "!")
+                {
+
+                    type = TOKEN_TYPE::TOK_BANG;
+                }
+
+                if (lexemes[i].lexeme == "!=")
+                {
+
+                    type = TOKEN_TYPE::TOK_BANG_EQUALS;
+                }
+
+                if (lexemes[i].lexeme == ">")
+                {
+
+                    type = TOKEN_TYPE::TOK_GREATER;
+                }
+
+                if (lexemes[i].lexeme == ">=")
+                {
+
+                    type = TOKEN_TYPE::TOK_GREATER_EQUALS;
+                }
+
+                if (lexemes[i].lexeme == "<")
+                {
+
+                    type = TOKEN_TYPE::TOK_LESS;
+                }
+
+                if (lexemes[i].lexeme == "<=")
+                {
+
+                    type = TOKEN_TYPE::TOK_LESS_EQUALS;
+                }
+
+                if (lexemes[i].lexeme == "if")
+                {
+
+                    type = TOKEN_TYPE::TOK_IF;
+                }
+
+                if (lexemes[i].lexeme == "fn")
+                {
+                    type = TOKEN_TYPE::TOK_FN;
+                }
+
+                if (lexemes[i].lexeme == "entry")
+                {
+                    type = TOKEN_TYPE::TOK_ENTRY;
+                }
+
+                if (lexemes[i].lexeme == "print")
+                {
+                    type = TOKEN_TYPE::TOK_PRINT;
+                }
+
+                if (type == TOKEN_TYPE::TOK_ERROR)
+                {
+                    type = TOKEN_TYPE::TOK_IDENTIFIER;
+                }
+
+                
+
+                tokens.push_back({type, lexeme_item});
             }
         };
 
